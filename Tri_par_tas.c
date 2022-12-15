40ee3cc4409a1b8523a585f7b3e650a08be49261
aya-celyne <59054943+aya-celyne@users.noreply.github.com>
2022-12-15 21:30:17 +0100

    Update Tri_par_tas.c

--- Tri_par_tas.c
+++ Tri_par_tas.c
@@ -46,13 +46,15 @@ void Aff_TabRevers(int T[],int T_revers[],int N)
     }
 }
 
-  void swap(int *a, int *b) {
+void swap(int *a, int *b) 
+  {
     int temp = *a;
     *a = *b;
     *b = temp;
   }
   
-  void retablir_tas(int arr[], int n, int i) {
+void retablir_tas(int arr[], int n, int i) 
+  {
     // Find largest among root, left child and right child
     int largest = i;
     int left = 2 * i + 1;
@@ -72,7 +74,11 @@ void Aff_TabRevers(int T[],int T_revers[],int N)
   }
   
   // Main function to do heap sort
-  void faireTas(int arr[], int n) {
+double faireTas(int arr[], int n) 
+  {
+    double temps;
+    clock_t start, stop;
+    start = clock();
     // Build max heap
     for (int i = n / 2 - 1; i >= 0; i--)
       retablir_tas(arr, n, i);
@@ -84,43 +90,33 @@ void Aff_TabRevers(int T[],int T_revers[],int N)
       // Heapify root element to get highest element at root again
       retablir_tas(arr, i, 0);
     }
+    stop =clock();
+    temps = (double)(stop-start)/CLOCKS_PER_SEC;
+    return temps;
   }
 
 int main()
 {
-    int N;
+    int N,i,j;
     double temps;
-    clock_t start, stop;   
     srand(time(NULL));
     printf("DONNEZ UNE TAILLE POUR LE TABLEAU QUE VOUS VOULEZ TRIER:");
     scanf("%d", &N);
     int * T= (int*)malloc(N*sizeof(int));
+    /*tableau trié inversement*/
     int * Tr= (int*)malloc(N*sizeof(int));
+    /*tableau trié*/
+    int * TT= (int*)malloc(N*sizeof(int));
     Gen_Tab(T,N);
-    printf("\n");
-    printf("TABLEAU AVANT LE TRI: \n");
-    Aff_Tab(T,N);
-    printf("\n\n");
-    printf("TABLEAU APRES LE TRI : \n");
-    start = clock();
-    faireTas(T,N);
-    stop = clock();
-    Aff_Tab (T,N);
-    Aff_TabRevers(T,Tr,N);
-    temps=(double)(stop-start)/CLOCKS_PER_SEC;
-    printf("\n\n");
-    printf("Temps d'execution 1 (Valeurs Aléatoires): %f", temps);
-    start = clock();
-    faireTas(Tr,N);
-    stop = clock();
-    temps=(double)(stop-start)/CLOCKS_PER_SEC;
-    printf("\nTemps d'execution 2 (Valeurs triées inversement): %f", temps);
-    start = clock();
-    faireTas(T,N);
-    stop = clock();
-    temps=(double)(stop-start)/CLOCKS_PER_SEC;
+    temps = faireTas(T,N);
+    printf("Temps d'execution 1 (Valeurs Aléatoires): %f \n", temps);
+    for (i = N-1, j=0; i >= 0; i--, j++)
+    Tr[j] = T[i];
+    for(i=0; i<N; i++) TT[i] = T[i];
+    temps = faireTas(Tr,N);
+    printf("\nTemps d'execution 2 (Valeurs triées inversement): %f \n", temps);
+    temps=faireTas(T,N);
     printf("\nTemps d'execution 3 (Valeurs triées): %f", temps);
     printf("\n\n");
-
     return 0;
 }
